12939ee63d81fc3dad9a24365d40bcb499cf5721
HomerJax <marcus.bofinger@gmail.com>
2026-07-30 19:03:44 +0000

    Persist Android WebView cookies

--- android/app/src/main/java/team/strikr/app/MainActivity.java
+++ android/app/src/main/java/team/strikr/app/MainActivity.java
@@ -1,5 +1,14 @@
 package team.strikr.app;
 
+import android.webkit.CookieManager;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onPause() {
+        CookieManager.getInstance().flush();
+        super.onPause();
+    }
+}
