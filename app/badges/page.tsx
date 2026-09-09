953b1a4981fde08cdfa8a3dd201cd89ce0b9a676
HomerJax <marcus.bofinger@gmail.com>
2026-09-09 10:43:35 +0200

    Wrap Hall of Fame in trophy-room styling

--- app/badges/page.tsx
+++ app/badges/page.tsx
@@ -1,3 +1,18 @@
 import "./badges.css";
+import "./trophy-room.css";
+import BadgesPageV3 from "./BadgesPageV3";
 
-export { default } from "./BadgesPageV3";
+type BadgesPageProps = {
+  searchParams?: Promise<{
+    player?: string;
+    compare?: string;
+  }>;
+};
+
+export default function BadgesPage(props: BadgesPageProps) {
+  return (
+    <div className="hall-of-fame-vitrine">
+      <BadgesPageV3 {...props} />
+    </div>
+  );
+}
